e44396bd95e3a09780e8ed575845ce26aaa32fa4
sebastianvcruz <sebastianvcruz@gmail.com>
2021-04-19 08:41:59 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,84 +22,147 @@ $(document).ready(function () {
   const labtalk = document.getElementById("labtalk");
 
   const texto3 = document.getElementById("textColumn3");
+  const dataGraph = document.getElementById("dataGraph");
+  const labguy_3_1 = document.getElementById("labguy_3_1");
+  const tube_3 = document.getElementById("tube_3");
+  const computer_3 = document.getElementById("computer_3");
+  const labguy_3_2 = document.getElementById("labguy_3_2");
+
+  const texto4 = document.getElementById("textColumn4");
+  const tube_4 = document.getElementById("tube_4");
+  const data_4 = document.getElementById("data_4");
+  const dataGraph_4 = document.getElementById("dataGraph_4");
+  const labguy_4 = document.getElementById("labguy_4");
+
+  const texto5 = document.getElementById("textColumn5");
+  const comp_5_1 = document.getElementById("comp_5_1");
+  const comp_5_2 = document.getElementById("comp_5_2");
+  const comp_5_3 = document.getElementById("comp_5_3");
+  const center_5 = document.getElementById("center_5");
 
   // const animateList = document.querySelectorAll("#project01 ");
-  let options = {
+  const options = {
     rootMargin: "0px",
-    threshold: 0.25
+    threshold: 0.4
   };
   observer = new IntersectionObserver((elms) => {
+    // let startLoad = true;
     elms.forEach((entry) => {
       if (entry.isIntersecting) {
         if (entry.target.id === "intro") {
           dataContainer.classList.add("moveToRight");
+          dataContainer.classList.remove("moveToLeft");
           gear.classList.remove("fadeIn");
           gear.classList.remove("fadeOut");
           dataA.classList.remove("fadeOut");
-          dataB.classList.remove("fadeIn");
+        } else {
+          dataContainer.classList.add("moveToLeft");
+          dataContainer.classList.remove("moveToRight");
+        }
+        if (entry.target.id === "project01") {
+          dataA.classList.add("fadeOut");
+          dataA.classList.remove("fadeIn");
+          dataB.classList.add("fadeIn");
+          dataB.classList.remove("fadeOut");
 
-        } else if (entry.target.id === "project01") {
           texto1.classList.add("fadeInLeft");
           labGuy.classList.add("fadeInRight");
           computer.classList.add("fadeInUp");
-          dataContainer.classList.add("moveToLeft");
-          dataContainer.classList.remove("moveToRight");
-          dataA.classList.add("fadeOut");
-          dataB.classList.add("fadeIn");
-          dataA.classList.remove("fadeIn");
-          dataB.classList.remove("fadeOut");
-          dataC.classList.remove("fadeIn");
-          dataC.classList.add("fadeOut");
           gear.classList.add("fadeIn");
           visuals.style.backgroundColor = "#F0F2F4";
-
-        } else if (entry.target.id === "project02") {
-          texto2.classList.add("fadeInLeft");
-          server.classList.add("fadeIn");
-          tube.classList.add("fadeInDown");
-          labtalk.classList.add("fadeInRight");
-          dataB.classList.add("fadeOut");
-          dataC.classList.add("fadeIn");
+        } else {
           dataB.classList.remove("fadeIn");
-          dataC.classList.remove("fadeOut");
-          dataD.classList.remove("fadeIn");
-          dataD.classList.add("fadeOut");
+          dataB.classList.add("fadeOut"); 
 
-        } else if (entry.target.id === "project03") {
-          texto3.classList.add("fadeInLeft");
-          dataD.classList.add("fadeIn");
-          dataC.classList.add("fadeOut");
-          dataC.classList.remove("fadeIn");
-          dataE.classList.remove("fadeIn");
-          
-        }else if (entry.target.id === "project04") {
-          dataE.classList.add("fadeIn");
-          dataD.classList.add("fadeOut");
-          dataD.classList.remove("fadeIn");
-          dataF.classList.remove("fadeIn");
-        }
-        else if (entry.target.id === "project05") {
-          dataF.classList.add("fadeIn");
-          dataE.classList.add("fadeOut");
-          dataE.classList.remove("fadeIn");
-        }
-        else {
           texto1.classList.remove("fadeInLeft");
           texto2.classList.remove("fadeInLeft");
           labGuy.classList.remove("fadeInRight");
           computer.classList.remove("fadeInUp");
-          dataContainer.classList.remove("moveToLeft");
-          dataA.classList.remove("fadeOut");
-          dataB.classList.remove("fadeIn");
-          // dataD.classList.remove("fadeIn");
         }
-        // entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
-      } else {
-        
-        // console.log(
-        //   "OFFFFF:",
-        //   entry
-        // )
+        if (entry.target.id === "project02") {
+          dataB.classList.add("fadeOut");
+          dataB.classList.remove("fadeIn");
+          dataC.classList.add("fadeIn");
+          dataC.classList.remove("fadeOut");
+
+          texto2.classList.add("fadeInLeft");
+          server.classList.add("fadeIn");
+          tube.classList.add("fadeInDown");
+          labtalk.classList.add("fadeInRight");
+        } else {
+          dataC.classList.remove("fadeIn");
+          dataC.classList.add("fadeOut");
+
+          texto2.classList.remove("fadeInLeft");
+          server.classList.remove("fadeIn");
+          tube.classList.remove("fadeInDown");
+          labtalk.classList.remove("fadeInRight");
+        }
+        if (entry.target.id === "project03") {
+          dataC.classList.add("fadeOut");
+          dataC.classList.remove("fadeIn");
+          dataD.classList.add("fadeIn");
+          dataD.classList.remove("fadeOut");
+
+          texto3.classList.add("fadeInLeft");
+          dataGraph.classList.add("fadeIn");
+          tube_3.classList.add("fadeInRight");
+          computer_3.classList.add("fadeInRight");
+          labguy_3_1.classList.add("fadeInUp");
+          labguy_3_2.classList.add("fadeInDown");
+        } else {
+          dataD.classList.remove("fadeIn");
+          dataD.classList.add("fadeOut");
+
+          texto3.classList.remove("fadeInLeft");
+          dataGraph.classList.remove("fadeIn");
+          tube_3.classList.remove("fadeInRight");
+          computer_3.classList.remove("fadeInRight");
+          labguy_3_1.classList.remove("fadeInUp");
+          labguy_3_2.classList.remove("fadeInDown");
+        }
+        if (entry.target.id === "project04") {
+          dataD.classList.add("fadeOut");
+          dataD.classList.remove("fadeIn");
+          dataE.classList.add("fadeIn");
+          dataE.classList.remove("fadeOut");
+
+          texto4.classList.add("fadeInLeft");
+          tube_4.classList.add("fadeInRight");
+          data_4.classList.add("fadeIn");
+          dataGraph_4.classList.add("fadeIn");
+          labguy_4.classList.add("fadeInLeft");
+        } else {
+          dataE.classList.remove("fadeIn");
+          dataE.classList.add("fadeOut");
+
+          texto4.classList.remove("fadeInLeft");
+          tube_4.classList.remove("fadeInRight");
+          data_4.classList.remove("fadeIn");
+          dataGraph_4.classList.remove("fadeIn");
+          labguy_4.classList.remove("fadeInLeft");
+        }
+        if (entry.target.id === "project05") {
+          dataE.classList.add("fadeOut");
+          dataE.classList.remove("fadeIn");
+          dataF.classList.add("fadeIn");
+          dataF.classList.remove("fadeOut");
+
+          texto5.classList.add("fadeInLeft");
+          comp_5_1.classList.add("fadeInDown");
+          comp_5_2.classList.add("fadeInDownB");
+          comp_5_3.classList.add("fadeInDownC");
+          center_5.classList.add("fadeInRight");
+        } else {
+          dataF.classList.remove("fadeIn");
+          dataF.classList.add("fadeOut");
+
+          texto5.classList.remove("fadeInLeft");
+          comp_5_1.classList.remove("fadeInDown");
+          comp_5_2.classList.remove("fadeInDownB");
+          comp_5_3.classList.remove("fadeInDownC");
+          center_5.classList.remove("fadeInRight");
+        }
       }
     });
   }, options);
@@ -224,6 +287,28 @@ $(document).ready(function () {
   //       100
   //     );
   //   });
+  let playing = false;
+  const sound = new Howl({
+    src: ["../audio/01_quality_assurance.wav"],
+    onplay: function () {
+      playing = true;
+      console.log("PLAYING?", sound.pos());
+    },
+    onpause: function () {
+      playing = false;
+    },
+    onend: function () {
+      console.log("END");
+      playing = false;
+      sound.stop();
+    }
+  });
+  playAudio = () => {
+    playing ? sound.pause() : sound.play();
+
+    // sound.play();
+  };
+  //
   cssScrollSnapPolyfill();
 
   console.log("LOADED");
